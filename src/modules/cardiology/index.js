e83e34d1ccb89428277e392012d8597990af85fc
cashdollar968-creator <cashdollar968@gmail.com>
2026-09-22 00:06:00 +0200

    Add cardiology module with initial properties

--- src/modules/cardiology/index.js
+++ src/modules/cardiology/index.js
@@ -1,1 +1,21 @@
+const cardiology = Object.freeze({
+  key: "cardiology",
 
+  name: "Cardiology",
+
+  clinicalDomain: "cardiology",
+
+  active: true,
+
+  departments: [],
+
+  units: [],
+
+  workflows: [],
+
+  capabilities: [],
+
+  routes: [],
+});
+
+export default cardiology;
