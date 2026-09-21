aba5fa9bf0281c7c857f077911f221328ba02bce
cashdollar968-creator <cashdollar968@gmail.com>
2026-09-21 23:59:18 +0200

    Implement module management in index.js
    Added module imports and functions to retrieve modules.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,1 +1,17 @@
+import cardiology from "./cardiology/index.js";
+import privateIcu from "./private-icu/index.js";
+import future from "./future/index.js";
 
+export const MODULES = Object.freeze({
+  cardiology,
+  privateIcu,
+  future,
+});
+
+export function getModule(moduleKey) {
+  return MODULES[moduleKey] || null;
+}
+
+export function getAllModules() {
+  return Object.values(MODULES);
+}
