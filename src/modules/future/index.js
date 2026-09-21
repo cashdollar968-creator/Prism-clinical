76db546ec0c5a71cd3d3a8171f29fd35167abc44
cashdollar968-creator <cashdollar968@gmail.com>
2026-09-22 00:09:16 +0200

    Add futureModules object with clinical domains

--- src/modules/future/index.js
+++ src/modules/future/index.js
@@ -1,1 +1,31 @@
+const futureModules = Object.freeze({
+  cardiacSurgery: {
+    key: "cardiac-surgery",
+    name: "Cardiac Surgery",
+    clinicalDomain: "cardiac-surgery",
+    active: false,
+  },
 
+  pediatricCardiacSurgery: {
+    key: "pediatric-cardiac-surgery",
+    name: "Pediatric Cardiac Surgery",
+    clinicalDomain: "pediatric-cardiac-surgery",
+    active: false,
+  },
+
+  renalTransplant: {
+    key: "renal-transplant",
+    name: "Renal & Kidney Transplantation",
+    clinicalDomain: "transplant",
+    active: false,
+  },
+
+  physiotherapyRehabilitation: {
+    key: "physiotherapy-rehabilitation",
+    name: "Physiotherapy & Rehabilitation",
+    clinicalDomain: "rehabilitation",
+    active: false,
+  },
+});
+
+export default futureModules;
