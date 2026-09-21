9d55f2b2469347ab39c0a0b5997ced6e4f165af2
cashdollar968-creator <cashdollar968@gmail.com>
2026-09-22 00:20:14 +0200

    Implement loadPRISMContext function
    Added loadPRISMContext function to load and structure context data.

--- src/core/context.js
+++ src/core/context.js
@@ -1,1 +1,81 @@
+import { loadTenantContext } from "./tenantContext.js";
+import {
+  getOrganization,
+  getOrganizationId,
+  getOrganizationName,
+} from "./organizationContext.js";
+import {
+  getCountry,
+  getCountryId,
+  getCountryCode,
+  getCountryName,
+} from "./countryContext.js";
+import {
+  getHospital,
+  getHospitalId,
+  getHospitalName,
+  getHospitalCode,
+} from "./hospitalContext.js";
+import {
+  getCampus,
+  getCampusId,
+  getCampusName,
+  getCampusCode,
+} from "./campusContext.js";
 
+export async function loadPRISMContext(
+  departmentId = null,
+  unitId = null
+) {
+  const context = await loadTenantContext(
+    departmentId,
+    unitId
+  );
+
+  return {
+    ...context,
+
+    organization: getOrganization(context),
+    country: getCountry(context),
+    hospital: getHospital(context),
+    campus: getCampus(context),
+
+    identifiers: {
+      organizationId:
+        getOrganizationId(context),
+
+      countryId:
+        getCountryId(context),
+
+      countryCode:
+        getCountryCode(context),
+
+      hospitalId:
+        getHospitalId(context),
+
+      hospitalCode:
+        getHospitalCode(context),
+
+      campusId:
+        getCampusId(context),
+    },
+
+    labels: {
+      organization:
+        getOrganizationName(context),
+
+      country:
+        getCountryName(context),
+
+      hospital:
+        getHospitalName(context),
+
+      campus:
+        getCampusName(context),
+    },
+  };
+}
+
+export function getPRISMContext(context) {
+  return context || null;
+      }
