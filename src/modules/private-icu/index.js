97733c6bbb9ea942efc901defa233c62b888a7bf
cashdollar968-creator <cashdollar968@gmail.com>
2026-09-22 00:07:38 +0200

    Add privateIcu object with initial properties

--- src/modules/private-icu/index.js
+++ src/modules/private-icu/index.js
@@ -1,1 +1,21 @@
+const privateIcu = Object.freeze({
+  key: "private-icu",
 
+  name: "Private ICU",
+
+  clinicalDomain: "critical-care",
+
+  active: true,
+
+  departments: [],
+
+  units: [],
+
+  workflows: [],
+
+  capabilities: [],
+
+  routes: [],
+});
+
+export default privateIcu;
